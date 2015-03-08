0f031421b7baf71962b286257c520cc5da0ced05
jackomayo <jackomayo@gmail.com>
2015-03-08 19:12:42 +0000

    formatting

--- main.cpp
+++ main.cpp
@@ -70,64 +70,96 @@ for(T=0.5;T<6;T+=0.02){
    
 //actual simulation
      for(i=0;i<Nc;i++){
-                                       
-       MyArray.choose();// select particle
-       Ei=MyArray.localEnergy();//sets initial energy
+       // select particle
+       MyArray.choose();
+       //sets initial energy
+       Ei=MyArray.localEnergy();
+
+       //Changes spin of selected particle
+       MyArray.perturb();
+
+       //Takes energy of selected particle and sets it to final energy
+       Ef=MyArray.localEnergy();
+
+       //Finds difference of final and initial energy
+       Edif=Ef-Ei;
      
-       MyArray.perturb();//Changes spin of selected particle
-       Ef=MyArray.localEnergy();//Takes energy of selected particle and sets it to final energy
-     
-       Edif=Ef-Ei;//Finds difference of final and initial energy
-     
-       r=((double) rand()/RAND_MAX);//Generates a random number between 0 and 1 and sets it to perturbation condition 
+       //Generates a random number between 0 and 1 and sets it to perturbation condition
+       r=((double) rand()/RAND_MAX);
       
-/*Compares perturbation condition to exp(-Edif/T), if perturbation condition is larger the selected particle's spin is changed back*/ 
+       //Compares perturbation condition to exp(-Edif/T), if perturbation condition is larger the selected particle's spin is changed back
        if (r>exp(-1*(Edif/T))) MyArray.perturb();
     
     
-//Data collection conditions given, data only collected from half of the Monte Carlo simulation, allowing the array to thermalise
-//aswell as only collected for every nth loop, where n is the size of the array, to avoid correlations
+       //Data collection conditions given, data only collected from half of the Monte Carlo simulation, allowing the array to thermalise
+       //aswell as only collected for every nth loop, where n is the size of the array, to avoid correlations
        if(i>Nc/2 && i%(ISINGSIZE*ISINGSIZE)==0){
-        MagSum+=MyArray.magnetisation();//Collects magnetisation data
-        Mag2Sum+=pow(MyArray.magnetisation(),2);//Collects magnetisation squared data
-        Esum+=MyArray.totalEnergy();//Collects energy data
-        E2sum+=pow(MyArray.totalEnergy(),2);//Collects energy squared data
-        DataCounter++;//Counts amount of data collected
-       }
+
+    	//Collects magnetisation data
+        MagSum+=MyArray.magnetisation();
+
+        //Collects magnetisation squared data
+        Mag2Sum+=pow(MyArray.magnetisation(),2);
+
+        //Collects energy data
+        Esum+=MyArray.totalEnergy();
+
+        //Collects energy squared data
+        E2sum+=pow(MyArray.totalEnergy(),2);
+
+        //Counts amount of data collected
+               }
+        DataCounter++;
+
+
      }
    
-    AvMag=MagSum/DataCounter;//Calculates average magnetisation 
-  
-    Av2Mag=Mag2Sum/DataCounter;//Calculates average magnetisation squared
-  
-    Eav=Esum/DataCounter;//Calculates average energy 
 
-    E2av=E2sum/DataCounter;//Calculates average energy squared
+     //Calculates average magnetisation
+     AvMag=MagSum/DataCounter;
+
+
+     //Calculates average magnetisation squared
+    Av2Mag=Mag2Sum/DataCounter;
+  
+    //Calculates average energy
+    Eav=Esum/DataCounter;
+
+    //Calculates average energy squared
+    E2av=E2sum/DataCounter;
  
-    CvSum+=(E2av-pow(Eav,2))/(pow(T,2)*pow(ISINGSIZE,2));//Collects specific heat
-    Cv2Sum+=pow((E2av-pow(Eav,2))/(pow(T,2)*pow(ISINGSIZE,2)),2);//Collects specific heat squared
+    //Collects specific heat
+    CvSum+=(E2av-pow(Eav,2))/(pow(T,2)*pow(ISINGSIZE,2));
+
+    //Collects specific heat squared
+    Cv2Sum+=pow((E2av-pow(Eav,2))/(pow(T,2)*pow(ISINGSIZE,2)),2);
 
   }
   
-  CvAv=CvSum/jmax;//Calculates the average specific heat
+   //Calculates the average specific heat
+   CvAv=CvSum/jmax;
   
-  Cv2Av=Cv2Sum/jmax;//Calculates the average specific heat squared
+   //Calculates the average specific heat squared
+   Cv2Av=Cv2Sum/jmax;
   
+   //Calculates specific heat error
+   Cverr=sqrt((Cv2Av-pow(CvAv,2))/jmax);
   
-  Cverr=sqrt((Cv2Av-pow(CvAv,2))/jmax);//Calculates specific heat error
+   //Calculates magnetisation error
+   Merr=sqrt((Av2Mag-pow(AvMag,2))/DataCounter);
   
-  Merr=sqrt((Av2Mag-pow(AvMag,2))/DataCounter);//Calculates magnetisation error
-  
-  
-  fprintf(results,"%lf %lf %lf %lf %lf\n",T,AvMag,Merr,CvAv,Cverr);//prints off data to specified text file
   
 
+   //output to file
+  fprintf(results,"%lf %lf %lf %lf %lf\n",T,AvMag,Merr,CvAv,Cverr);
+  
+//reset array
   MyArray.reset();
  }
  
  fclose(results);
  
- printf("done!\n");   
+ printf("cooked!\n");
 
  return 0;    
 }
